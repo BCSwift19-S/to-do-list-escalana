94ed9d33252338f9c92e729fac851b3c77077d91
Alessandra Escalante <escalana@bc.edu>
2019-02-09 17:54:41 -0500

    Initial Commit

---
+++ To Do List App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List App
+//
+//  Created by Ale Escalante on 2/9/19.
+//  Copyright © 2019 Ale Escalante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
