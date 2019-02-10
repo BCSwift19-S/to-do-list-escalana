67288f4ded1a51fcf1ab3f4fd35a000fa784106f
Alessandra Escalante <escalana@bc.edu>
2019-02-10 13:24:40 -0500

    5.3 complete

--- To Do List App/ViewController.swift
+++ To Do List App/ViewController.swift
@@ -20,8 +20,29 @@ class ViewController: UIViewController {
         tableview.delegate = self
         tableview.dataSource = self
     }
-
-
+    // setting up the segue from my tableview(so my viewController?) to my DetailViewController
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EditItem"{
+            let destination = segue.destination as! DetailViewController
+            let index = tableview.indexPathForSelectedRow!.row
+            destination.toDoItem = toDoArray[index]
+        }else{
+            if let selectedPath = tableview.indexPathForSelectedRow {
+                tableview.deselectRow(at: selectedPath, animated: false)
+            }
+        }
+    }
+    @IBAction func unwindFromDetailViewController(segue:UIStoryboardSegue){
+        let sourceViewController = segue.source as! DetailViewController
+        if let indexPath = tableview.indexPathForSelectedRow{
+            toDoArray[indexPath.row] = sourceViewController.toDoItem!
+            tableview.reloadRows(at: [indexPath], with: .automatic)
+        }else{
+            let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
+            toDoArray.append(sourceViewController.toDoItem!)
+            tableview.insertRows(at: [newIndexPath], with: .automatic)
+        }
+    }
 }
 extension ViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
