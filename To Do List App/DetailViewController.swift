40249a0c635cadb1dc27c8d98684318f720efddc
Alessandra Escalante <escalana@bc.edu>
2019-02-11 09:37:43 -0500

    5.6 -- the previous commit was 5.5

--- To Do List App/DetailViewController.swift
+++ To Do List App/DetailViewController.swift
@@ -11,14 +11,19 @@ import UIKit
 class DetailViewController: UIViewController {
    
     @IBOutlet weak var toDoField: UITextField!
-    var toDoItem: String?
  
+    @IBOutlet weak var toDoNoteView: UITextView!
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    var toDoItem: String?
+    var toDoNoteItem: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if let toDoItem = toDoItem{
             toDoField.text = toDoItem
+        }
+        if let toDoNoteItem = toDoNoteItem{
+            toDoNoteView.text = toDoNoteItem
         }
         enableDisableSaveButton()
         toDoField.becomeFirstResponder() 
--- To Do List App/ViewController.swift
+++ To Do List App/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var addBarButton: UIBarButtonItem!
+    var toDoNotesArray = ["I should be certain to do all the exercises at the end of the chapters before the exam", "Take myideas to the school's venture competition and win the big check", "Focus apps on empowerment for all, with extra hours for the users who are kind"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +31,7 @@ class ViewController: UIViewController {
             let destination = segue.destination as! DetailViewController
             let index = tableview.indexPathForSelectedRow!.row
             destination.toDoItem = toDoArray[index]
+            destination.toDoNoteItem = toDoNotesArray[index]
         }else{
             if let selectedPath = tableview.indexPathForSelectedRow {
                 tableview.deselectRow(at: selectedPath, animated: false)
@@ -40,10 +42,12 @@ class ViewController: UIViewController {
         let sourceViewController = segue.source as! DetailViewController
         if let indexPath = tableview.indexPathForSelectedRow{
             toDoArray[indexPath.row] = sourceViewController.toDoItem!
+            toDoNotesArray[indexPath.row] = sourceViewController.toDoNoteItem!
             tableview.reloadRows(at: [indexPath], with: .automatic)
         }else{
             let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
             toDoArray.append(sourceViewController.toDoItem!)
+            toDoNotesArray.append(sourceViewController.toDoNoteItem!)
             tableview.insertRows(at: [newIndexPath], with: .automatic)
         }
     }
@@ -60,12 +64,15 @@ class ViewController: UIViewController {
     }
 }
 extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return toDoArray.count
+    
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = toDoArray[indexPath.row]
+        cell.detailTextLabel?.text = toDoNotesArray[indexPath.row]
         return cell
     }
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -76,8 +83,11 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
     }
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let itemToMove = toDoArray[sourceIndexPath.row]
+        let noteToMove = toDoNotesArray[sourceIndexPath.row]
         toDoArray.remove(at: sourceIndexPath.row)
+        toDoNotesArray.remove(at: sourceIndexPath.row)
         toDoArray.insert(itemToMove, at: destinationIndexPath.row)
+        toDoNotesArray.insert(noteToMove, at: destinationIndexPath.row)
     
     }
     
